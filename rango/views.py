8b819ccf42384e4992417d59643b3b23eb03f443
ZhihanLiu16 <2559703L@student.gla.ac.uk>
2021-07-30 22:30:28 +0800

    chapter9-logout

--- rango/views.py
+++ rango/views.py
@@ -11,6 +11,8 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
@@ -134,3 +136,8 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
