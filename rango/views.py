37e8c80c6d671955c194a143a4965aa774396a6d
ZhihanLiu16 <2559703L@student.gla.ac.uk>
2021-07-30 22:25:50 +0800

    chapter9-restrict

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,7 @@ from django.urls import reverse
 
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -129,3 +130,7 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
